8dfc1cd7d1f3d9a7189e831693654763266e3aee
Assil Ksiksi <aksiksi@gatech.edu>
2017-10-10 19:50:48 -0400

    Swap bytes instead of words!

--- client/includes/utils.hpp
+++ client/includes/utils.hpp
@@ -8,7 +8,10 @@ union {
     uint8_t bytes[4];
 } IntSplitter;
 
-inline uint32_t swap_words(const uint32_t& value) {
-    // Swaps the two 16-bit words in a unsigned 32-bit dword
-    return ((value & 0x0000FFFF) << 0x10) | ((value & 0xFFFF0000) >> 0x10);
+inline uint32_t swap_bytes(const uint32_t& value) {
+    // Swaps the the ordering of bytes in a 4 byte word
+    return ((value & 0x000000FF) << 24) | 
+           ((value & 0x0000FF00) << 8)  |
+           ((value & 0x00FF0000) >> 8)  |
+           ((value & 0xFF000000) >> 24);
 }
